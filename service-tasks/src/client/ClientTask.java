6a75bdb9c541b4adc9655f45bafe053c96b8fcf5
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2023-04-25 15:14:04 -0300

    feat: improve client task

--- service-tasks/src/client/ClientTask.java
+++ service-tasks/src/client/ClientTask.java
@@ -12,15 +12,57 @@ public class ClientTask {
 
         System.out.println("Connection established");
 
-        PrintStream output = new PrintStream(socket.getOutputStream());
-        output.println("c1");
+        Thread threadSendCommand = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    PrintStream output = new PrintStream(socket.getOutputStream());
+                    Scanner keyboard = new Scanner(System.in);
+                    while(keyboard.hasNextLine()) {
+                        String line = keyboard.nextLine();
 
-        Scanner keyboard = new Scanner(System.in);
+                        if(line.trim().equals("")) {
+                            break;
+                        }
 
-        keyboard.nextLine();
+                        output.println(line);
+                    }
 
-        output.close();
-        keyboard.close();
+                    output.close();
+                    keyboard.close();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread threadReceiveResponse = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("Received server data");
+                    Scanner responseServer = new Scanner(socket.getInputStream());
+
+                    while (responseServer.hasNextLine()) {
+                        String line = responseServer.nextLine();
+                        System.out.println(line);
+                    }
+
+                    responseServer.close();
+
+
+                }catch (Exception e) {
+
+                }
+            }
+        });
+
+        threadSendCommand.start();
+        threadReceiveResponse.start();
+
+        threadSendCommand.join(); //thread main wait this thread finished
+
+        System.out.println("Close client socket");
         socket.close();
     }
 }
