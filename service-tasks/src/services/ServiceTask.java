507d8b9781d645b7d0d283655b88657b5eb3b474
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2023-04-26 19:32:43 -0300

    chore: refactor tasks

--- service-tasks/src/services/ServiceTask.java
+++ service-tasks/src/services/ServiceTask.java
@@ -4,25 +4,45 @@ import utils.DistributeTasks;
 
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.Executor;
+import java.net.SocketException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ServiceTask {
 
-    public static void main(String[] args) throws Exception {
+    private ExecutorService threadPool;
+    private ServerSocket server;
+    private boolean isRunning;
 
+    public ServiceTask() throws Exception {
         System.out.println("---- Inicialized server ----");
-        ServerSocket service = new ServerSocket(12345);
-//        ExecutorService threadPool = Executors.newFixedThreadPool(2); //created pool and defined max connections
-        ExecutorService threadPool = Executors.newCachedThreadPool();//created dynamic connections, close when not used
+        this.server = new ServerSocket(12345);
+        this.threadPool = Executors.newCachedThreadPool();
+        this.isRunning = true;
+    }
 
-        while (true) {
-            Socket socket = service.accept();
-            System.out.println("Accept new client on port: " + socket.getPort());
-
-            DistributeTasks distributeTasks = new DistributeTasks(socket); //new thread
-            threadPool.execute(distributeTasks);
+    public void runServer() throws Exception {
+        while (this.isRunning) {
+            try {
+                Socket socket = server.accept();
+                System.out.println("Accept new client on port: " + socket.getPort());
+                DistributeTasks distributeTasks = new DistributeTasks(socket, this); //new thread
+                threadPool.execute(distributeTasks);
+            } catch (SocketException ex) {
+                System.out.println("SocketException, running? " + this.isRunning);
+            }
         }
+    }
+
+    public void stopServer() throws Exception {
+        isRunning = false;
+        server.close();
+        threadPool.shutdown();
+    }
+
+    public static void main(String[] args) throws Exception {
+        ServiceTask serviceTask = new ServiceTask();
+        serviceTask.runServer();
+        serviceTask.stopServer();
     }
 }
